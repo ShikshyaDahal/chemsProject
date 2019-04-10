9531a131e738ceaa359f80e489930653f929e55e
Part Jaithong <lightcompass@gmail.com>
2019-04-10 23:59:36 +0700

    Fix input height and dismiss keyboard on login page

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,33 +4,42 @@ import {
   Text,
   View,
   TextInput,
-  TouchableOpacity 
+  TouchableOpacity ,
+  Keyboard,
+  TouchableWithoutFeedback
 } from 'react-native';
+
+var DismissKeyboard = require('dismissKeyboard'); // Require React Native's utility library.
+
 
 export default class Form extends Component{
 
 	render(){
 		return(
-			<View style={styles.container}>
-          <TextInput style={styles.inputBox} 
-              underlineColorAndroid='rgba(0,0,0,0)' 
-              placeholder="Email"
-              placeholderTextColor = "#ffffff"
-              selectionColor="#fff"
-              keyboardType="email-address"
-              onSubmitEditing={()=> this.password.focus()}
-              />
-          <TextInput style={styles.inputBox} 
-              underlineColorAndroid='rgba(0,0,0,0)' 
-              placeholder="Password"
-              secureTextEntry={true}
-              placeholderTextColor = "#ffffff"
-              ref={(input) => this.password = input}
-              />  
-           <TouchableOpacity style={styles.button}>
-             <Text style={styles.buttonText}>{this.props.type}</Text>
-           </TouchableOpacity>     
-  		</View>
+			
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <View style={styles.container}>
+            <TextInput style={styles.inputBox} 
+                underlineColorAndroid='rgba(0,0,0,0)' 
+                placeholder="Email"
+                placeholderTextColor = "#ffffff"
+                selectionColor="#fff"
+                keyboardType="email-address"
+                onSubmitEditing={()=> this.password.focus()}
+                />
+            <TextInput style={styles.inputBox} 
+                underlineColorAndroid='rgba(0,0,0,0)' 
+                placeholder="Password"
+                secureTextEntry={true}
+                placeholderTextColor = "#ffffff"
+                ref={(input) => this.password = input}
+                />  
+             <TouchableOpacity style={styles.button}>
+               <Text style={styles.buttonText}>{this.props.type}</Text>
+             </TouchableOpacity>    
+            </View>
+           </TouchableWithoutFeedback>
+  		
 			)
 	}
 }
@@ -44,6 +53,7 @@ const styles = StyleSheet.create({
 
   inputBox: {
     width:300,
+    height:40,
     backgroundColor:'rgba(255, 255,255,0.2)',
     borderRadius: 25,
     paddingHorizontal:16,
